a57f43905c20425715c81e453d8979920ce34bf3
LucaJ <Luca.Franken@student.hu.nl>
2021-03-03 14:01:11 +0100

    6b, opdr 2

--- src/practicum6/a/Main.java
+++ src/practicum6/a/Main.java
@@ -1,6 +1,7 @@
 package practicum6.a;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
@@ -13,14 +14,21 @@ public class Main {
         Persoon p1 = new Persoon("Eric", 200);
         Persoon p2 = new Persoon("Hans", 55);
 
-
         System.out.println("p1 koopt g1:" + (p1.koop(g1) ? "" : " niet") + " gelukt");
         System.out.println("p1 koopt g2:" + (p1.koop(g2) ? "" : " niet") + " gelukt");
         System.out.println("p1 koopt g3:" + (p1.koop(g3) ? "" : " niet") + " gelukt");
+
         System.out.println("\np1: " +p1+ "\n\np2: " +p2+ "\n");
 
         System.out.println("p1 verkoopt g2 aan p2:"+(p1.verkoop(g2, p2) ? "" : " niet")+" gelukt");
         System.out.println("p1 verkoopt g1 aan p2:"+(p1.verkoop(g1, p2) ? "" : " niet")+" gelukt");
         System.out.println("\np1: " +p1+ "\n\np2: " +p2+ "\n");
+
+        ArrayList<Game> teKoop = new ArrayList<Game>();
+        teKoop.add(g1); // false
+        teKoop.add(new Game("Mario Kart 8", 2019, 35.00)); // true
+        ArrayList<Game> nogNietInBezit = p1.bepaalGamesNietInBezit(teKoop); //[mario]
+        System.out.println("p1 heeft de volgende games nog niet: " + nogNietInBezit.toString());
+
     }
 }
